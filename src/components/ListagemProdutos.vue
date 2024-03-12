a7efe2da4f40c7a3d4bec0b9fb93d1ff611c2135
Raizinhoo <gersinhowasd@gmail.com>
2024-03-12 16:36:13 -0300

    Menu Superior Feito

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -27,35 +27,56 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
     </div>
   </template>
 
-  <style>
-    .container {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 1rem;
-        justify-content: center;
-        align-items: center;
-        margin: auto;
-        padding: 1rem 0;
-    }
-    .card {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        flex-direction: column;
-        width: 15rem;
-        height: 25rem;
-        background: #fff;
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23.);
-    }
-    .card--title {
-        color: #222;
-        font-weight: 700;
-        text-transform: capitalize;
-        font-size: 1.1rem;
-        margin-top: 0,5;
-    }
-    
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  padding: 1rem 0;
+}
+.card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 15rem;
+  height: 25rem;
+  background: #fff;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 10px;
+  margin: auto;
+  overflow: hidden;
+}
+.card--avatar {
+  width: 100%;
+  height: 17rem;
+  object-fit: cover;
+  margin-bottom: 0.5rem;
+}
+.card--title {
+  color: #222;
+  font-weight: 700;
+  text-transform: capitalize;
+  font-size: 1.1rem;
+  margin-top: 0.5rem;
+}
 
+<style scoped > @media (max-width: 768px) {
+  .container {
+    gap: 0.5rem;
+  }
+  .card {
+    width: 92%;
+  }
+}
 
+@media (min-width: 768px) and (max-width: 1024px) {
+  .card {
+    width: 22rem;
+  }
+}
 
 </style>
