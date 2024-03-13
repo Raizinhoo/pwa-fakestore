76a310eeebcab0571f84e8f51f1d02c1f4a0dd1f
Raizinhoo <gersinhowasd@gmail.com>
2024-03-13 16:44:18 -0300

    teste da proporção de tela

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -2,6 +2,10 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
+import { useScreen } from '@/composables/screen';
+
+const { browserWidth, deviceWidth, isMobile} = useScreen();
+
 const produtos = ref([]);
 
 onMounted(async () => {
@@ -15,15 +19,12 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
 
 <template>
     <div>
-      <h1>Produtos</h1>
-      <div class="container">
-        <div class="card" v-for="produto in produtos" :key="produto.id">
-          <h1 class="card--title">{{ produto.title }}</h1>
-          <p>{{ produto.description }}</p>
-          <p>{{ formatPrice(produto.price) }}</p>
-          <img class="card--avatar" :src="produto.image" :alt="produto.title" />
-        </div>
-      </div>
+      <h1> Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{ isMobile }}
+        <span v-if="isMobile"> é Celular</span>
+      </h1>
+      
+      
+
     </div>
   </template>
 
