ede1a55213b0006e776756ac114b017198808258
Raizinhoo <gersinhowasd@gmail.com>
2024-03-13 16:47:42 -0300

    volta ao normal no listagem produtos

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -19,12 +19,15 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
 
 <template>
     <div>
-      <h1> Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{ isMobile }}
-        <span v-if="isMobile"> é Celular</span>
-      </h1>
-      
-      
-
+      <h1>Produtos</h1>
+      <div class="container">
+        <div class="card" v-for="produto in produtos" :key="produto.id">
+          <h1 class="card--title">{{ produto.title }}</h1>
+          <p>{{ produto.description }}</p>
+          <p>{{ formatPrice(produto.price) }}</p>
+          <img class="card--avatar" :src="produto.image" :alt="produto.title" />
+        </div>
+      </div>
     </div>
   </template>
 
